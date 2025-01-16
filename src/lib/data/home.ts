ad4a6708ed6dae87575f5bbed06dd43a813afcc8
Louis <lp280500@falmouth.ac.uk>
2025-01-16 23:43:43 +0000

    red-fin: Added About me

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -11,7 +11,7 @@ const hero: {
 } = {
 	title: `${BaseData.fullName},`,
 	description:
-		'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+		'I have always been drawn to problem-solving, attention to detail, and logical thinking. When I first found programming in school, I wrote it off and saw it as another chore or homework that had to be completed for class. I started Computer Science A level and made a simple Python calculator script, which completely changed my initial thoughts about programming. I discovered the potential of programming and how I can use programming to immerse myself in problem-solving and logical exploration. Since then, my passion and life goal has been to program new algorithms and methods. Beyond Programming, I love rock climbing, badminton, and spending time with friends; I also enjoy learning about life, reflecting on its complexities, and figuring out how to live it.',
 	links: [
 		{ label: 'GitHub', href: 'https://github.com/Louis-Palmer', icon: 'i-carbon-logo-github' },
 		{ label: 'LinkedIn', href: 'https://www.linkedin.com/in/louis-palmer-447a1524b/', icon: 'i-carbon-logo-linkedin' },
