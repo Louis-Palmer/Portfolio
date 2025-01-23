1f64714cc8ac54386b09aeb9362f8b416c4b81ea
Louis <lp280500@falmouth.ac.uk>
2025-01-23 09:03:35 +0000

    Added Resume

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -3,7 +3,7 @@ import { href } from '$lib/utils';
 const title = 'Resumé';
 
 // const resume = href('/pdf/resume.pdf');
-const resume = href('pdf/LouisPalmer_CV.pdf');
+const resume = href('pdf/LouisPalmer_Resume.pdf');
 
 const ResumeData = { title, resume };
 
