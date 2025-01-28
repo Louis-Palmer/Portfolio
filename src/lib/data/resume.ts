e4f994d9bd4de5e43dd013dedf87726f9070e46b
Louis-Palmer <louispalmer01@outlook.com>
2025-01-28 17:35:41 +0000

    ref-wip: Added WIP resume

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -3,7 +3,7 @@ import { href } from '$lib/utils';
 const title = 'Resumé';
 
 // const resume = href('/pdf/resume.pdf');
-const resume = href('pdf/LouisPalmer_Resume.pdf');
+const resume = href('pdf/LOUIS PALMER, CV Q1 V3, 28-1-25.pdf');
 
 const ResumeData = { title, resume };
 
