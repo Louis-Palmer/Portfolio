d7a774de05d0df11243a70d82a4bd1b95774f459
Louis-Palmer <louispalmer01@outlook.com>
2025-02-10 00:52:18 +0000

    ref-fin: Updated CV

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -3,7 +3,7 @@ import { href } from '$lib/utils';
 const title = 'Curriculum Vitae';
 
 // const resume = href('/pdf/resume.pdf');
-const resume = href('pdf/LOUIS PALMER, CV Q1 V5, 31-1-25.pdf');
+const resume = href('pdf/LOUIS PALMER, CV Q1 V6, 10-2-25.pdf');
 
 const ResumeData = { title, resume };
 
